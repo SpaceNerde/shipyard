aec30a4c353324e98201bb8bf8c14829c5ee0697
SpaceNerde <spacenerddev@gmail.com>
2025-09-13 22:47:55 +0200

    parse yaml from markdown metadata

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
 use std::fs;
 
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Event, Options, Parser, Tag, TagEnd};
+use saphyr::{LoadableYamlNode, Yaml};
 use tera::Tera;
 
+fn get_metadata(data: &mut String) -> Option<String> {
+    if !data.starts_with("---") {
+        return None;
+    }
+
+    let start = data.find("---").unwrap_or(0);
+    data.replace_range(start..3, "");
+    let end = data.find("---").unwrap_or(data.len());
+
+    let metadata = data[start..end].trim().to_owned();
+    Some(metadata)
+}
+
 fn main() {
-    let file = fs::read_to_string("test/test_file.md").unwrap();
+    let mut file = fs::read_to_string("test/test_file.md").unwrap();
     let template_file = fs::read_to_string("test/test_template.html").unwrap();
-    
+
     // Parse Markdown to data
+    // Get the metadata
+    let mut metadata_buffer = get_metadata(&mut file).unwrap();
+    let yaml = Yaml::load_from_str(&metadata_buffer).unwrap();
+
+    // Get the rest of the HTML
     let mut options = Options::empty();
     options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
 
-    let mut parser = Parser::new_ext(file.trim(), options);
-
-    println!("{:?}", &parser);
-
+    let parser = Parser::new_ext(file.trim(), options);
+  
     let mut html_buffer = String::new();
     html::push_html(&mut html_buffer, parser);
 
-    print!("{}", html_buffer);
-
     // Insert Data into template and save into file
-    let mut tera = Tera::new("test/**/*").unwrap();
+    let tera = Tera::new("test/**/*").unwrap();
 
     let mut ctx = tera::Context::new();
     ctx.insert("title", "TEST");
