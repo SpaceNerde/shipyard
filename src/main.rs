2e0357176f4170ed9fe6bfd6b054aa0389c25846
SpaceNerde <spacenerddev@gmail.com>
2025-09-14 10:14:32 +0200

    start extracting data from metadata block and insert into template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use pulldown_cmark::{html, Event, Options, Parser, Tag, TagEnd};
-use saphyr::{LoadableYamlNode, Yaml};
+use saphyr::{LoadableYamlNode, Yaml, YamlEmitter};
 use tera::Tera;
 
 fn get_metadata(data: &mut String) -> Option<String> {
@@ -23,8 +23,15 @@ fn main() {
 
     // Parse Markdown to data
     // Get the metadata
-    let mut metadata_buffer = get_metadata(&mut file).unwrap();
+    let mut metadata_buffer = get_metadata(&mut file.clone()).unwrap();
+
     let yaml = Yaml::load_from_str(&metadata_buffer).unwrap();
+
+    let title = yaml[0].as_mapping().unwrap().get_key_value(
+        &Yaml::Value(saphyr::Scalar::String(std::borrow::Cow::Borrowed("title")))
+    ).unwrap().1.as_str().unwrap();
+
+    println!("{:?}", title);
 
     // Get the rest of the HTML
     let mut options = Options::empty();
@@ -35,11 +42,12 @@ fn main() {
     let mut html_buffer = String::new();
     html::push_html(&mut html_buffer, parser);
 
+
     // Insert Data into template and save into file
     let tera = Tera::new("test/**/*").unwrap();
 
     let mut ctx = tera::Context::new();
-    ctx.insert("title", "TEST");
+    ctx.insert("title", title);
     ctx.insert("body_html", &html_buffer);
 
     let rendered = tera.render("test_template.html", &ctx).unwrap();
